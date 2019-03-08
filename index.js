91760cf414a04229e88cfb3550630485c8e90bba
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2019-03-08 12:33:52 -0800

    update form

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 var app = express();
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
-
+/* 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/form', (req, res) => res.render('pages/form'))
   .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`)) */
