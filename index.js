dcc63bcdb3726d03d81b49ab7a5cefc01108a8b9
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2019-03-08 15:43:29 -0800

    update .js file

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const cool = require('cool-ascii-faces')
+
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000
 
-var app = express();
 app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
 
-/* 
-express()
+
+// start the server listening
+app.listen(port, function() {
+  console.log('Node app is running on port', port);
+});
+
+/* express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`)) */
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))  */
